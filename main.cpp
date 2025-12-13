70c4fbb775ef6d7d780586a0e28f6863a03009fa
Archead <55419973+archead@users.noreply.github.com>
2025-12-13 12:02:19 -0800

    add vertex description structs

--- main.cpp
+++ main.cpp
@@ -50,6 +50,17 @@ static std::vector<char> readFile(const std::string& filename) {
 struct Vertex {
 	glm::vec2 pos;
 	glm::vec3 color;
+
+	static vk::VertexInputBindingDescription getBindingDescription() {
+		return {0, sizeof(Vertex), vk::VertexInputRate::eVertex};
+	}
+
+	static std::array<vk::VertexInputAttributeDescription, 2> getAttributeDescriptions() {
+		return{
+			vk::VertexInputAttributeDescription(0, 0, vk::Format::eR32G32Sfloat, offsetof(Vertex, pos)),
+			vk::VertexInputAttributeDescription(1, 0, vk::Format::eR32G32B32Sfloat, offsetof(Vertex, color))
+		};
+	}
 };
 
 const std::vector<Vertex> vertices = {
