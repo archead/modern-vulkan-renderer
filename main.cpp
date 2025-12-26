70df32ea5c85e9621fc0d74b7ab70ed0ffed6b77
Archead <55419973+archead@users.noreply.github.com>
2025-12-26 13:51:14 -0800

    implement updateUniformBuffer()

--- main.cpp
+++ main.cpp
@@ -11,10 +11,12 @@
 #include <cstdint> // Necessary for uint32_t
 #include <limits> // Necessary for std::numeric_limits
 #include <algorithm> // Necessary for std::clamp
+#include <chrono>
 
 #include <fstream>
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 constexpr uint32_t WIDTH = 800;
 constexpr uint32_t HEIGHT = 600;
@@ -834,6 +836,8 @@ private:
 
 		vk::PipelineStageFlags waitDestinationStageMask(vk::PipelineStageFlagBits::eColorAttachmentOutput);
 
+		updateUniformBuffer(imageIndex);
+
 		vk::SubmitInfo submitInfo;
 
 		submitInfo.waitSemaphoreCount = 1;
@@ -858,6 +862,24 @@ private:
 		}
 
 		currentFrame = (currentFrame + 1) % MAX_FRAMES_IN_FLIGHT;
+	}
+
+	void updateUniformBuffer(uint32_t imageIndex) {
+		static auto startTime = std::chrono::high_resolution_clock::now();
+
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		float time = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - startTime).count();
+
+		UniformBufferObject ubo = {};
+		ubo.model = rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		ubo.proj = glm::perspective(
+			glm::radians(45.0f),
+			static_cast<float>(swapChainExtent.width) / static_cast<float>(swapChainExtent.height),
+			0.0f, 10.0f);
+		ubo.proj[1][1] *= -1;
+
+		memcpy(uniformBuffersMapped[imageIndex], &ubo, sizeof(ubo));
 	}
 };
 
