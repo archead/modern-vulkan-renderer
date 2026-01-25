5caa729dafd0805bf1817da913b3011213b311e9
Archead <55419973+archead@users.noreply.github.com>
2026-01-25 13:11:23 -0800

    implement loadModel()

--- main.cpp
+++ main.cpp
@@ -1115,6 +1115,28 @@ private:
 		if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, MODEL_PATH.c_str())) {
 			throw std::runtime_error(warn + err);
 		}
+
+		for (const auto& shape : shapes) {
+			for (const auto& index : shape.mesh.indices) {
+				Vertex vertex{};
+
+				vertex.pos = {
+					attrib.vertices[3 * index.vertex_index + 0],
+					attrib.vertices[3 * index.vertex_index + 1],
+					attrib.vertices[3 * index.vertex_index + 2],
+				};
+
+				vertex.texCoord = {
+					attrib.texcoords[2 * index.texcoord_index + 0],
+					1.0f - attrib.texcoords[2 * index.texcoord_index + 1],
+				};
+
+				vertex.color = {1.0f, 1.0f, 1.0f};
+
+				vertices.push_back(vertex);
+				indices.push_back(indices.size()); // this does basically the same thing as int i++;
+			}
+		}
 	}
 
 	void initVulkan() {
